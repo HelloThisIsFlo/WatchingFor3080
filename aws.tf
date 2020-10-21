53215d8dfff0a6e21f3b76453d95826829f99329
Florian Kempenich <Flori@nKempenich.com>
2020-10-21 16:20:44 +0100

    Add bucket policy to TF config

--- aws.tf
+++ aws.tf
@@ -14,5 +14,22 @@ provider "aws" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "watch-for-3080"
-  acl = "public-read"
+  acl    = "public-read"
+  policy = <<POLICY
+{
+  "Id": "AllowWriteFor_WatchFor3080_User",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowWrite",
+      "Action": ["s3:PutObject", "s3:PutObjectAcl"],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::watch-for-3080/*",
+      "Principal": {
+        "AWS": ["arn:aws:iam::193208114745:user/WatchFor3080"]
+      }
+    }
+  ]
+}
+POLICY
 }
