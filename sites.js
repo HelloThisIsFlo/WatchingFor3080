d3de639967d7ca265aa25b094e14c5a69f67ed83
Florian Kempenich <Flori@nKempenich.com>
2020-10-21 03:05:20 +0100

    Tweak wait durations

--- sites.js
+++ sites.js
@@ -3,13 +3,13 @@ module.exports = {
     url:
       "https://www.nvidia.com/en-gb/shop/geforce/?page=1&limit=9&locale=en-gb&category=GPU&gpu=RTX%203080",
     selector: "featured-product",
-    waitDuration: 1500,
+    waitDuration: 3000,
   },
   nvidiaStoreFR: {
     url:
       "https://www.nvidia.com/fr-fr/shop/geforce/gpu/?page=1&limit=9&locale=fr-fr&category=GPU&gpu=RTX%203080",
     selector: "featured-product",
-    waitDuration: 1500,
+    waitDuration: 3000,
   },
   // scanGB: {
   //   url: "https://www.scan.co.uk/shops/nvidia/3080-founders-edition",
